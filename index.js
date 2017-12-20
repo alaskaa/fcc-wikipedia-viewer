6057b1f589185755b932ecd536cdf3cd3198d0f4
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-20 22:11:47 +0100

    Unsaved changes from index.js

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ $.ajax({
 });
 
 // https://stackoverflow.com/questions/36985111/using-wikipedias-api-to-fetch-results-from-search-query
+
+// $(document).ready(function(){
+//      $('#btn').click(function() {
+//          $.ajax({
+//              url: 'http://en.wikipedia.org/w/api.php',
+//              data: { action: 'query', list: 'search', srsearch: $("input[name=Wikipedia]").val(), format: 'json' },
+//              dataType: 'jsonp',
+//              success: processResult
+//          });
+//      });
+//  });
+
+// function processResult(apiResult){
+//   for (var i = 0; i < apiResult.query.search.length; i++){
+//        $('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
+//   }
+// }
