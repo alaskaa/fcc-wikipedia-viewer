cf5e17c1b29c82e83cda65219b73d072a4e3e2da
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-22 14:42:19 +0100

    update index.js to work with icon to query the API and make icon smaller

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-$.ajax({
-  url: 'http://en.wikipedia.org/w/api.php',
-  data: {
-    action: 'query',
-    list: 'search',
-    srsearch: 'soup',
-    format: 'json',
-    formatversion: 2
-  },
-  dataType: 'jsonp',
-  success: function (x) {
-    console.log('title', x.query.search[0].title);
-  }
+$(document).ready(function(){
+     $('span').click(function(e) {
+       e.preventDefault();
+       $.ajax({
+         url: 'http://en.wikipedia.org/w/api.php',
+         data: {
+         action: 'query',
+         list: 'search',
+         srsearch: document.getElementById('searchstring').value, // here I need to input the query parameter
+         format: 'json',
+         formatversion: 2
+       },
+       dataType: 'jsonp',
+       success: function (x) {
+         console.log(x);
+         console.log('title', x.query.search[0].title); // this is what gets returned on success
+       }
+     });
+   });
 });
+
+//function ali(){
+  //alert("You sure you want to search this?");
+  //console.log(window.location.search);
+//}
 
 // https://stackoverflow.com/questions/36985111/using-wikipedias-api-to-fetch-results-from-search-query
 
