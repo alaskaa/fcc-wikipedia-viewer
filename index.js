39f1fbf41b43e3e5cca3a1136e6354edd7203243
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-22 23:37:21 +0100

    Update index.js to spit out list of search terms and link to urls

--- index.js
+++ index.js
@@ -11,34 +11,18 @@ $(document).ready(function(){
          formatversion: 2
        },
        dataType: 'jsonp',
-       success: function (x) {
-         console.log(x);
-         console.log('title', x.query.search[0].title); // this is what gets returned on success
-       }
+       success: processResult
      });
    });
 });
 
-//function ali(){
-  //alert("You sure you want to search this?");
-  //console.log(window.location.search);
-//}
-
-// https://stackoverflow.com/questions/36985111/using-wikipedias-api-to-fetch-results-from-search-query
-
-// $(document).ready(function(){
-//      $('#btn').click(function() {
-//          $.ajax({
-//              url: 'http://en.wikipedia.org/w/api.php',
-//              data: { action: 'query', list: 'search', srsearch: $("input[name=Wikipedia]").val(), format: 'json' },
-//              dataType: 'jsonp',
-//              success: processResult
-//          });
-//      });
-//  });
-
-// function processResult(apiResult){
-//   for (var i = 0; i < apiResult.query.search.length; i++){
-//        $('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
-//   }
-// }
+function processResult(apiResult){
+   console.log(apiResult);
+   for (var i = 0; i < apiResult.query.search.length; i++){
+        let url = "http://en.wikipedia.org/?curid="+apiResult.query.search[i].pageid;
+        $('#display-result').append('<a href="'+url+'">'+apiResult.query.search[i].title+'</a>'+'<br>');
+        //$('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
+        //$('#display').append('<p>'+url+'</p>');
+        console.log(url);
+   }
+}
