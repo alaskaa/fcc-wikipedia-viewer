ea33cd534bbadb2cdee8f4ee5768cd6a61540db2
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-23 14:54:05 +0100

    fix refresh issues for new search and html and body issues

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ $(document).ready(function(){
 });
 
 function processResult(apiResult){
+   $('#display-result').empty();
    console.log(apiResult);
-   for (var i = 0; i < apiResult.query.search.length; i++){
-        let url = "http://en.wikipedia.org/?curid="+apiResult.query.search[i].pageid;
-        $('#display-result').append('<a href="'+url+'"target="_blank">'+apiResult.query.search[i].title+'</a>'+'<br>');
+   for (var i = 0; i < apiResult.query.search.length; i++) {
+     let url = "http://en.wikipedia.org/?curid="+apiResult.query.search[i].pageid;
+     $('#display-result').append('<a href="'+url+'"target="_blank">'+'<h3>'+apiResult.query.search[i].title+'</h3>'+'<br>'+apiResult.query.search[i].snippet+'</a>'+'<br><br>');
         //$('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
         //$('#display').append('<p>'+url+'</p>');
-        console.log(url);
+     console.log(url);
    }
+   document.body.classList.add('marginiser');
 }
